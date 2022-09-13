0975bf4055fa260d0bdb2b6df96a950a1cf2f45f
E. Junior <evando66.ea@gmail.com>
2022-09-13 00:20:23 -0300

    feat: add firebase config and first methods

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,53 @@
 import { createServer } from 'http'
+// import { initializeApp, cert } from 'firebase-admin/app'
+import admin from 'firebase-admin'
+import { QuerySnapshot, DocumentData, DocumentSnapshot, WriteResult } from 'firebase-admin/firestore'
+
 import cors from 'cors'
 
 import Server from './api/server'
 
+const firebaseConfig = require('../nodeApiSecretKey.json')
+
 try {
+
+    admin.initializeApp({
+        credential: admin.credential.cert(firebaseConfig),
+        databaseURL: 'https://data-base-name.firebaseio.com'
+    })
+
+    const firestore = admin.firestore()
+
+    // create new collection and new doc
+    firestore.collection('users').doc().create({
+        rapahel: 'nome certo RAFAEL'
+    }).then((data: WriteResult) => {
+        console.log('object create with sucess', data);
+        
+    })
+
+    // add new data to a collection
+    firestore.collection('test-collection').add({ newTest: 'this is one test on firestore' }).then((documentRef) => {
+        console.log(`Added document at ${documentRef.path})`);
+    });
+
+
+    // Show all documents data from a collection
+    firestore.collection('test-collection').get().then((data: QuerySnapshot) => {
+        data.forEach(documentSnp => {
+            console.log(documentSnp.data());
+        })
+    })
+
+    // show the data from a specific document
+    firestore
+        .collection('fwdd7dLeTrpn0sMjMih9')
+        .doc('AVpHSxOHXzbWh694EB55')
+        .get()
+        .then((data: DocumentSnapshot) => {
+            console.log(data.data());
+        })
+
     const app = new Server().app
     app.use(cors({
         origin: ['http://localhost:3000'] // change the port to front-end port
